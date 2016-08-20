6802dbd022deaf42e00687ccbc1f50041a1cdcdb
PETEroid <selllfocard@gmail.com>
2016-08-19 23:25:17 -0700

    update example

--- src/components/Main.js
+++ src/components/Main.js
@@ -49,7 +49,6 @@ var Main = React.createClass({
               ratios={this.state.ratio2}
               size={200}
               fill="#0f0"
-              isAnimating={false}
               className="my-polygon-2"/>
             <Polygon n={this.props.n} 
               ratios={this.state.ratio} 
