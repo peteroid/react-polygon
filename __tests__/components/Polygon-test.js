c7953faf964d271f3ffe0b97e9a88df8eeecca26
PETEroid <selllfocard@gmail.com>
2017-01-05 16:19:09 +0800

    #2 add tests for polygon

--- __tests__/components/Polygon-test.js
+++ __tests__/components/Polygon-test.js
@@ -11,3 +11,31 @@ test('Polygon is rendered properly with default props', () => {
   let tree = polygon.toJSON()
   expect(tree).toMatchSnapshot()
 })
+
+test('Polygon is rendered properly with different props', () => {
+  const sides = [3, 4, 5, 6, 7, 8, 9, 10, 12, 24, 48]
+  const sizes = [50, 144, 169, 5354, 9007199254740991]
+  const fills = ['#ffffff', '#000000', '#123ABC', '#DEF456']
+  const ratioFuncs = [
+    n => Array.apply(null, Array(n)).map(i => 1),
+    n => Array.apply(null, Array(n)).map(i => 0),
+    n => {
+      Array.apply(null, Array(n)).map(i => {
+        parseInt(Math.sin(i) * 10000) / 10000
+      })
+    }
+  ]
+
+  let polygons = []
+  sides.forEach(n => sizes.forEach(size => fills.forEach(fill => ratioFuncs.forEach(ratioFunc => {
+    let props = {n, size, fill, ratios: ratioFunc(n)}
+    polygons.push(renderer.create(
+      <Polygon {...props} />
+    ))
+  }))))
+
+  let trees = polygons.map(p => p.toJSON())
+  trees.forEach(t => {
+    expect(t).toMatchSnapshot()
+  })
+})
